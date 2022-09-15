336aa01bfc51cc7248db8df54ea21efc5ec37c6c
nntto <nattou-king@hotmail.co.jp>
2022-09-15 17:44:26 +0900

    firebase.json更新

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -8,6 +8,7 @@ import {
   InputAdornment,
   Select,
   MenuItem,
+  Typography,
 } from "@mui/material";
 import React from "react";
 import { useForm, useFieldArray } from "react-hook-form";
@@ -16,6 +17,9 @@ import {
   Add as AddIcon,
   DeleteOutline as DeleteOutlineIcon,
 } from "@mui/icons-material";
+import ConciergeCard from "../molecules/ConciergeCard";
+import { concierges } from "../data/concierges";
+import { useLocation } from "react-router-dom";
 
 type FormValues = {
   hasBudget: boolean;
@@ -55,137 +59,155 @@ const Order: React.FC = () => {
 
   const onSubmit = (data: FormValues) => console.log(data);
 
+  const search = useLocation().search;
+  const query = new URLSearchParams(search);
+  const conciergeId = query.get("conciergeId");
+
   return (
-    <Container maxWidth="sm" sx={{ pt: 5 }}>
-      <Stack spacing={2}>
-        <Select
-          labelId="hasBudget"
-          size="small"
-          id="hasBudget"
-          {...register("hasBudget", {})}
-        >
-          <MenuItem value={true as any}>予算あり</MenuItem>
-          <MenuItem value={false as any}>予算無制限</MenuItem>
-        </Select>
-        {fields.map((field, index) => {
-          return (
-            // 必ず fields オブジェクトの id をコンポーネントの key に割り当てる
-            <React.Fragment key={field.id}>
-              <Stack spacing={2}>
-                <Box display="flex">
-                  <TextField
-                    sx={{ mr: 2, flex: 4 }}
-                    size="small"
-                    label="品名"
-                    // arrays/array フィールドを使用する場合、input の name 属性を name[index] のように割り当てることができる
-                    {...register(`items.${index}.material`, {
-                      required: true,
-                    })}
-                  />
-                  <TextField
-                    sx={{ mr: 1, flex: 3 }}
-                    size="small"
-                    type="string"
-                    label="量"
-                    {...register(`items.${index}.amount`, {
-                      required: true,
-                    })}
-                  />
-                  {watchHasBudget ? (
+    <>
+      {conciergeId !== null ? (
+        <ConciergeCard
+          concierge={concierges[conciergeId]}
+          conciergeId={conciergeId}
+        />
+      ) : (
+        <></>
+      )}
+      <Container maxWidth="sm" sx={{ pt: 5 }}>
+        <Stack spacing={2}>
+          <Typography variant="h4">注文</Typography>
+          <Select
+            labelId="hasBudget"
+            size="small"
+            id="hasBudget"
+            {...register("hasBudget", {})}
+          >
+            <MenuItem value={true as any}>予算あり</MenuItem>
+            <MenuItem value={false as any}>予算無制限</MenuItem>
+          </Select>
+          {fields.map((field, index) => {
+            return (
+              // 必ず fields オブジェクトの id をコンポーネントの key に割り当てる
+              <React.Fragment key={field.id}>
+                <Stack spacing={2}>
+                  <Box display="flex">
                     <TextField
-                      sx={{ flex: 2 }}
+                      sx={{ mr: 2, flex: 4 }}
+                      size="small"
+                      label="品名"
+                      // arrays/array フィールドを使用する場合、input の name 属性を name[index] のように割り当てることができる
+                      {...register(`items.${index}.material`, {
+                        required: true,
+                      })}
+                    />
+                    <TextField
+                      sx={{ mr: 1, flex: 3 }}
                       size="small"
                       type="string"
-                      label="予算"
-                      InputProps={{
-                        startAdornment: (
-                          <InputAdornment position="start">￥</InputAdornment>
-                        ),
-                      }}
-                      {...register(`items.${index}.budget`, {})}
+                      label="量"
+                      {...register(`items.${index}.amount`, {
+                        required: true,
+                      })}
                     />
-                  ) : (
-                    <></>
-                  )}
-                  <IconButton aria-label="delete" onClick={() => remove(index)}>
-                    <DeleteOutlineIcon />
-                  </IconButton>
+                    {watchHasBudget ? (
+                      <TextField
+                        sx={{ flex: 2 }}
+                        size="small"
+                        type="string"
+                        label="予算"
+                        InputProps={{
+                          startAdornment: (
+                            <InputAdornment position="start">￥</InputAdornment>
+                          ),
+                        }}
+                        {...register(`items.${index}.budget`, {})}
+                      />
+                    ) : (
+                      <></>
+                    )}
+                    <IconButton
+                      aria-label="delete"
+                      onClick={() => remove(index)}
+                    >
+                      <DeleteOutlineIcon />
+                    </IconButton>
+                  </Box>
+                  <TextField
+                    sx={{ mr: 2, flex: 1 }}
+                    type="string"
+                    label="要望"
+                    {...register(`items.${index}.comment`, {
+                      maxLength: 1000,
+                    })}
+                  >
+                    円
+                  </TextField>
+                </Stack>
+                <Box color="error.main" fontSize={12}>
+                  {/* ErrorMessage コンポーネントは name 属性で関連付けされた入力のエラーメッセージを表示するためのシンプルなコンポーネント */}
+                  <ErrorMessage
+                    errors={errors}
+                    name={`items.${index}.material`}
+                    message="⚠ 品名を入力してください"
+                    as="p"
+                  />
+                  <ErrorMessage
+                    errors={errors}
+                    name={`items.${index}.material`}
+                    message="⚠ 量欄を入力してください"
+                    as="p"
+                  />
+                  <ErrorMessage
+                    errors={errors}
+                    name={`items.${index}.comment`}
+                    message="⚠ 要望欄は1000文字以下です"
+                    as="p"
+                  />
                 </Box>
-                <TextField
-                  sx={{ mr: 2, flex: 1 }}
-                  type="string"
-                  label="要望"
-                  {...register(`items.${index}.comment`, {
-                    maxLength: 1000,
-                  })}
-                >
-                  円
-                </TextField>
-              </Stack>
-              <Box color="error.main" fontSize={12}>
-                {/* ErrorMessage コンポーネントは name 属性で関連付けされた入力のエラーメッセージを表示するためのシンプルなコンポーネント */}
-                <ErrorMessage
-                  errors={errors}
-                  name={`items.${index}.material`}
-                  message="⚠ 品名を入力してください"
-                  as="p"
-                />
-                <ErrorMessage
-                  errors={errors}
-                  name={`items.${index}.material`}
-                  message="⚠ 量欄を入力してください"
-                  as="p"
-                />
-                <ErrorMessage
-                  errors={errors}
-                  name={`items.${index}.comment`}
-                  message="⚠ 要望欄は1000文字以下です"
-                  as="p"
-                />
-              </Box>
-            </React.Fragment>
-          );
-        })}
-      </Stack>
-      <Button
-        // MUI v5 から sx props で Button コンポーネントにも直接スタイルを書けるようになった
-        sx={{ mt: 1 }}
-        startIcon={<AddIcon />}
-        // append 関数はフィールドの最後に input を追加する
-        onClick={() =>
-          append({
-            material: "",
-            amount: "",
-            budget: 0,
-            comment: "",
-          })
-        }
-      >
-        行を追加する
-      </Button>
-      <Box textAlign="center" mt={2}>
+              </React.Fragment>
+            );
+          })}
+        </Stack>
         <Button
-          variant="outlined"
-          sx={{ mr: 1 }}
+          // MUI v5 から sx props で Button コンポーネントにも直接スタイルを書けるようになった
+          sx={{ mt: 1 }}
+          startIcon={<AddIcon />}
+          // append 関数はフィールドの最後に input を追加する
           onClick={() =>
-            reset({
-              items: [{ material: "", amount: "", budget: 0, comment: "" }],
+            append({
+              material: "",
+              amount: "",
+              budget: 0,
+              comment: "",
             })
           }
         >
-          リセット
+          行を追加する
         </Button>
-        <Button
-          color="primary"
-          variant="contained"
-          disableElevation
-          // submit イベントからバリデーションがトリガーされる
-          onClick={handleSubmit(onSubmit)}
-        >
-          注文
-        </Button>
-      </Box>
-    </Container>
+        <Box textAlign="center" mt={2}>
+          <Button
+            variant="outlined"
+            sx={{ mr: 1 }}
+            onClick={() =>
+              reset({
+                items: [{ material: "", amount: "", budget: 0, comment: "" }],
+              })
+            }
+          >
+            リセット
+          </Button>
+          <Button
+            color="primary"
+            variant="contained"
+            disableElevation
+            // submit イベントからバリデーションがトリガーされる
+            onClick={handleSubmit(onSubmit)}
+          >
+            注文
+          </Button>
+        </Box>
+      </Container>
+    </>
   );
 };
 
